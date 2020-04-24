18c643eed7170696e9cfe1b5b99094bc00ff55b4
Lvchangze <18302010026@fudan.edu.cn>
2020-04-24 15:52:45 +0800

    wx：for

--- miniprogram/pages/secondHand/secondHand.js
+++ miniprogram/pages/secondHand/secondHand.js
@@ -3,8 +3,27 @@ const app = getApp()
 
 Page({
   data: {
+    recommendGoods:[
+      {
+        goodName:"娃娃",
+        goodPrice:"30元",
+        goodReleaseTime:"2020/3/20",
+        goodSeller:"吕昌泽",
+        goodIntroduction:"无",
+        goodPicture:"",
+      },
+      {
+        goodName:"大娃娃",
+        goodPrice:"50元",
+        goodReleaseTime:"2020/3/25",
+        goodSeller:"吕昌泽",
+        goodIntroduction:"哈哈哈",
+        goodPicture:""
+      }
+    ]
   },
   
+
   onShow: function () {
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().setData({
